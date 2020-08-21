c74fa35f7e4c56ca00b84747abe3cee154b810e4
Valentin Courgeau <valentin.courgeau@free.fr>
2020-08-21 15:15:31 +0100

    add tests with testthat

---
+++ R/custom_mle.R
@@ -0,0 +1,67 @@
+CustomLikelihood <- function(data, parametrisation='standard'){
+  # returns a function of parameters
+
+  return(function(par){
+    data_for_mle <- data
+    p <- length(which(data_for_mle>0)) / length(data_for_mle)
+
+    if(parametrisation == 'standard'){
+      kap <- (1-p^{par[1]})*par[2]/abs(par[1])
+      p_non_zero <- 1-(1+kap/(par[2]/abs(par[1])-kap))^{-1/par[1]}
+    }else{
+      kap <- (1-p^{1/3.0})*par[2]/abs(1/3.0)
+      p_non_zero <- 1-(1+kap/(par[2]/abs(1/3.0)-kap))^{-3.0}
+    }
+
+    # p_non_zero <- 1-(1+kap/(par[2]/abs(par[1])-kap))^{-1/par[1]}
+    like <- evir::dgpd(data_for_mle[data_for_mle>0.0], beta = par[2], xi=par[1],
+                       mu=0) * p_non_zero
+    log.like <- sum(log(like)) + length(data_for_mle == 0.0) * log(1-p_non_zero)
+    log.like <- min(log.like, 1e9)
+    log.like <- max(log.like, -1e9)
+    return(-log.like)
+  })
+}
+
+
+CompositeLikelihood <- function(data, parametrisation='standard'){
+  # returns a function of parameters
+
+  return(function(par){
+    data_for_mle <- data
+
+    data_for_mle[data_for_mle > 0] <- -log(1+par[1]*(par[3] + data_for_mle[data_for_mle > 0])/par[2])/par[1]
+    data_for_mle[data_for_mle == 0.0] <- -log(1-(1+par[1]*(par[3])/par[2])^{-1/par[1]})
+    return(mean(data))
+
+  })
+}
+
+
+CustomMarginalMLE <- function(data, parametrisation='standard'){
+  length_data <- min(20000, length(data))
+  init_guess <- as.numeric(evir::gpd(data[1:length_data], threshold = 0)$par.ests)
+  cat('custom MLE init_guess', init_guess, '\n')
+  fn_mle <- CustomLikelihood(data = data, parametrisation = parametrisation)
+
+  lower <- c(1e-03, 0.1)
+  if(parametrisation == 'std_trf'){
+    lower <- c(-2, 0.1)
+  }
+  res <- stats::optim(par = init_guess[1:2], fn_mle, method='L-BFGS-B', lower=lower, upper=c(2,20))$par
+  return(res)
+}
+
+
+CompositeMarginalMLE <- function(data, parametrisation='standard'){
+  init_guess <- as.numeric(evir::gpd(data, threshold = 0)$par.ests)
+  print(init_guess)
+  fn_mle <- CompositeLikelihood(data = data, parametrisation = parametrisation)
+
+  lower <- c(1e-03, 0.1)
+  if(parametrisation == 'std_trf'){
+    lower <- c(-2, 0.1)
+  }
+
+  return(stats::optim(par = init_guess[1:2], fn_mle, method='L-BFGS-B', lower=lower, upper=c(2,20))$par)
+}
