7bdc899abf438ccb13f38e2955afdbfabb077c68
Valentin Courgeau <valentin.courgeau@free.fr>
2020-08-22 17:10:54 +0100

    add custom mle tests and implemented transformation

--- R/custom_mle.R
+++ R/custom_mle.R
@@ -1,61 +1,84 @@
+GetInitialGuessAndBounds <- function(data, max_length=20000, minus_mult=0.7, plus_multiplier=1.3){
+  length_data <- min(max_length, length(data))
+  data <- data[1:length_data]
+
+  init_guess <- as.numeric(evir::gpd(data, threshold = 0, method="pwm")$par.ests) # (xi, sigma)
+
+  p <- length(which(data>0)) / length(data)
+  kap <- (1 - p) / p
+  init_guess <- c(init_guess, kap)
+  minus_init <- minus_mult * init_guess
+  plus_init <- plus_multiplier * init_guess
+  bds <- cbind(minus_init, plus_init)
+  lower <- apply(bds, 1, min)
+  upper <- apply(bds, 1, max)
+
+  return(list(init_guess=init_guess, lower=lower, upper=upper))
+}
+
 CustomLikelihood <- function(data){
+  # with 0 and non-0
   # returns a function of parameters
 
   return(function(par){
     data_for_mle <- data
     p <- length(which(data_for_mle>0)) / length(data_for_mle)
 
-    kap <- (1-p^{par[1]})*par[2]/abs(par[1])
-    p_non_zero <- 1-(1+kap/(par[2]/abs(par[1])-kap))^{-1/par[1]}
+    kap <- par[3]
+    p_zero <- 1 - 1. / (1. + kap)
 
+    like <- evir::dgpd(data_for_mle[data_for_mle>0.0], beta = par[2], xi=par[1], mu=0.0) * (1-p_zero)
+    like <- like[!is.na(like)] + 1e-7
+    log.like <- sum(log(like)) + length(which(data_for_mle == 0.0)) * log(p_zero)
 
-    like <- evir::dgpd(data_for_mle[data_for_mle>0.0],
-                       beta = par[2], xi=par[1], mu=0.0) * p_non_zero
-    log.like <- sum(log(like)) + length(data_for_mle == 0.0) * log(1-p_non_zero)
     log.like <- min(log.like, 1e9)
     log.like <- max(log.like, -1e9)
+
     return(-log.like)
   })
 }
 
 
-CompositeLikelihood <- function(data, parametrisation='standard'){
-  # returns a function of parameters
-  stop('CompositeLikelihood')
-  return(function(par){
-    data_for_mle <- data
-
-    data_for_mle[data_for_mle > 0] <- -log(1+par[1]*(par[3] + data_for_mle[data_for_mle > 0])/par[2])/par[1]
-    data_for_mle[data_for_mle == 0.0] <- -log(1-(1+par[1]*(par[3])/par[2])^{-1/par[1]})
-    return(mean(data))
-  })
-}
-
-
 CustomMarginalMLE <- function(data, parametrisation='standard'){
-  length_data <- min(20000, length(data))
-  init_guess <- as.numeric(evir::gpd(data[1:length_data], threshold = 0)$par.ests)
-  cat('custom MLE init_guess', init_guess, '\n')
-  fn_mle <- CustomLikelihood(data = data, parametrisation = parametrisation)
+  get_init_guess <- GetInitialGuessAndBounds(data)
+  init_guess <- get_init_guess$init_guess
+  lower <- get_init_guess$lower
+  upper <- get_init_guess$upper
+  fn_mle <- CustomLikelihood(data = data)
 
-  lower <- c(1e-03, 0.1)
-  if(parametrisation == 'std_trf'){
-    lower <- c(-2, 0.1)
-  }
-  res <- stats::optim(par = init_guess[1:2], fn_mle, method='L-BFGS-B', lower=lower, upper=c(2,20))$par
+  res <- stats::optim(par = init_guess[1:3], fn_mle, method='L-BFGS-B', lower=lower, upper=upper)$par
   return(res)
 }
 
 
-CompositeMarginalMLE <- function(data, parametrisation='standard'){
-  init_guess <- as.numeric(evir::gpd(data, threshold = 0)$par.ests)
-  print(init_guess)
-  fn_mle <- CompositeLikelihood(data = data, parametrisation = parametrisation)
+CompositeLikelihood <- function(data){
+  # implements custom likelihood with jacobian
+  # returns a function of parameters
+  return(function(par){
+    data_for_mle <- data
 
-  lower <- c(1e-03, 0.1)
-  if(parametrisation == 'std_trf'){
-    lower <- c(-2, 0.1)
-  }
+    p_non_zero <- 1. / (1. + par[3])
 
-  return(stats::optim(par = init_guess[1:2], fn_mle, method='L-BFGS-B', lower=lower, upper=c(2,20))$par)
+    trf_data <- TransformationMapInverse(data_for_mle, params_std=par)
+    custom_lk <- CustomLikelihood(data = trf_data) # needs to inject transformed data here
+    trf_lk <- custom_lk(par = c(1., 1. + par[3], par[3])) # GPD(1, 1+kappa)
+
+    jacob <- TransformationJacobian(params_std = par)
+    jacob_vals <- jacob(data_for_mle)
+    jacob_vals <- -sum(log(jacob_vals[!is.na(jacob_vals)] + 1e-7))
+
+    composite_lk <- jacob_vals + trf_lk
+    return(composite_lk)
+  })
+}
+
+
+CompositeMarginalMLE <- function(data){
+  get_init_guess <- GetInitialGuessAndBounds(data)
+  init_guess <- get_init_guess$init_guess
+  lower <- get_init_guess$lower
+  upper <- get_init_guess$upper
+  fn_mle <- CompositeLikelihood(data = data)
+
+  return(stats::optim(par = init_guess[1:3], fn_mle, method='L-BFGS-B', lower=lower, upper=upper, control = list())$par)
 }
