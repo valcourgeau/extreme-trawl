a5f72a61642d3a60709f6556d8781e9a3b76837b
Valentin Courgeau <valentin.courgeau@free.fr>
2020-08-22 17:26:20 +0100

    add trawl families

---
+++ R/trawl_gamma.R
@@ -0,0 +1,26 @@
+GammaTrawl <- new.env()
+
+# param is (alpha, H)
+
+GammaTrawl$LebA <- function(param){
+  return(param[1]/(param[2]-1))
+}
+
+GammaTrawl$TrawlB1 <- function(param, h){
+  return(SupIGTrawl$LebA(param)-GammaTrawl$TrawlB2(param, h))
+}
+
+
+GammaTrawl$TrawlB2 <- function(param, h){
+  return(exp((1-param[2])*log(1+h/param[1]))*GammaTrawl$LebA(param))
+}
+
+
+GammaTrawl$TrawlB3 <- function(param, h){
+  return(SupIGTrawl$TrawlB1(param, h))
+}
+
+GammaTrawl$Config <- function(){
+  return(list(n_params=2, lower=c(0.5, 2.0), upper=c(3,4)))
+}
+
---
+++ R/trawl_sum_exponential.R
@@ -0,0 +1,41 @@
+SumExponentialTrawl <- new.env()
+
+SumExponentialTrawl$SumExp <- function(param, h, trawl_f){
+  n_sup <- length(param)
+  exp_trawl_params <- seq(from=0.01, to=1, length.out = n_sup)
+  trawl_val <- vapply(exp_trawl_params, function(par){
+    trawl_f(param = par, h)
+  }, rep(1.0, length(h)))    
+  
+  trawl_val <- trawl_val * matrix(rep(param/sum(param), length(h)), ncol=n_sup, byrow = T)
+  return(rowSums(trawl_val))
+}
+
+SumExponentialTrawl$TrawlB1 <- function(param, h){
+  return(SumExponentialTrawl$SumExp(param, h, ExponentialTrawl$TrawlB1))
+}
+
+# must have 
+# ExponentialTrawl$TrawlB1(1.0, 0:10)
+# equal to 
+# SumExponentialTrawl$TrawlB1(c(1.0), h=0:10)
+
+
+SumExponentialTrawl$TrawlB2 <- function(param, h){
+  return(SumExponentialTrawl$SumExp(param, h, ExponentialTrawl$TrawlB2))
+}
+
+# ExponentialTrawl$TrawlB2(1.0, 0:10)
+# SumExponentialTrawl$TrawlB2(c(1.0), h=0:10)
+
+SumExponentialTrawl$TrawlB3 <- function(param, h){
+  return(SumExponentialTrawl$SumExp(param, h, ExponentialTrawl$TrawlB3))
+}
+
+# ExponentialTrawl$TrawlB3(1.0, 0:10)
+# SumExponentialTrawl$TrawlB3(c(1.0), h=0:10)
+
+SumExponentialTrawl$Config <- function(){
+  n_params <- 4
+  return(list(n_params=n_params, lower=rep(1e-3, n_params), upper=rep(1.0, n_params)))
+}
