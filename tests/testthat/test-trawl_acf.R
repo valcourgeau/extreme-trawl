a9c48ceec12d098ca282bbb611bd46756f6f2213
Valentin Courgeau <valentin.courgeau@free.fr>
2020-08-30 12:57:02 +0100

    acf trawl renamed

--- tests/testthat/test-trawl_acf.R
+++ tests/testthat/test-trawl_acf.R
@@ -6,11 +6,44 @@ test_that("trawl acf", {
   rho <- .2
   # print(acf_trawl_inv(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho))
   a <- acf_trawl(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho)
-  b <- acf_trawl_revised(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho)
-  # print(microbenchmark::microbenchmark(acf_trawl(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho), times=5))
-  # print(microbenchmark::microbenchmark(acf_trawl_revised(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho), times=5))
+  b <- AcfTrawl(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho)
 
   testthat::expect_equal(a, b, tolerance=1e-3)
-  # print(FirstMoment(c(1,2), .1, 10., -.3, -1.4))
-  # print(CrossMoment(c(1,2), .1, 10., -.3, -1.4))
 })
+
+test_that("trawl acf time", {
+  TIME_DIVISOR <- 1000000
+  h <- 1
+  alpha <- 2.
+  beta <- 10.
+  kappa <- 19.
+  rho <- .2
+
+  time_old <- microbenchmark::microbenchmark(acf_trawl(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho), times=5)$time / TIME_DIVISOR
+  time_new <- microbenchmark::microbenchmark(AcfTrawl(h=h, alpha=alpha, beta=beta, kappa=kappa, rho=rho), times=5)$time / TIME_DIVISOR
+
+  testthat::expect_equal(time_new/time_old, rep(0., length(time_old)), tolerance=.15)
+})
+
+test_that("collection trawl acf", {
+  TIME_DIVISOR <- 1000000
+  h <- 1
+  alpha <- 2.
+  beta <- 10.
+  kappa <- 19.
+  rho <- .2
+
+  h_collection <- 1:20
+
+  time_old <- microbenchmark::microbenchmark(
+    acf_trawl_num_approx(h=h_collection, alpha=alpha, beta=beta, kappa=kappa, rho=rho),
+    times=5)$time / TIME_DIVISOR
+  time_new <- microbenchmark::microbenchmark(
+    AcfTrawlCollection(h=h_collection, alpha=alpha, beta=beta, kappa=kappa, rho=rho),
+    times=5)$time / TIME_DIVISOR
+
+  testthat::expect_equal(time_new/time_old, rep(0., length(time_old)), tolerance=.15)
+})
+
+
+
