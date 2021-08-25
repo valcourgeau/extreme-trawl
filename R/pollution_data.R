93fd1536565d744c9af5d7c285e6cacd6cf8ad34
Valentin Courgeau <v.courgeau@instadeep.com>
2021-08-25 11:51:53 +0100

    feat: add dataset and desc

---
+++ R/pollution_data.R
@@ -0,0 +1,18 @@
+#' Air Pollutant Concentration on Marylebone Road, London, UK.
+#'
+#' A dataset containing the air pollutant concentration of six molecules:
+#' Ozone (O3), Carbon Monoxide (CO), Nitric Oxide (NO), Nitric Dioxide (NO2),
+#' Particulate Matter below 10mm (PM10), Sulphur Dioxide (SO2).
+#'
+#' @format A data frame with 157,710 rows and 6 variables:
+#' \describe{
+#'   \item{O3}{Ozone air concentration, in µg/m3}
+#'   \item{CO}{Carbon Monoxyde air concentration, in µg/m3}
+#'   \item{NO}{Nitric Oxide air concentration, in µg/m3}
+#'   \item{NO2}{Nitric Dioxide air concentration, in µg/m3}
+#'   \item{PM10}{Particulate Matter below 10mm concentration, in µg/m3}
+#'   \item{SO2}{Sulphur Dioxide air concentration, in µg/m3}
+#' }
+#' @source \url{
+#'    https://uk-air.defra.gov.uk/data/site-data?f_site_id=MY1&view=last_hour
+#' }
