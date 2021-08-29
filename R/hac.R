6771005dac8a58d54b7be8e04acedca338148345
Valentin Courgeau <val.courgeau@gmail.com>
2021-08-29 15:20:34 +0100

    feat: docs for hac

--- R/hac.R
+++ R/hac.R
@@ -1,8 +1,35 @@
+
+#' Autocovariance matrix from vector.
+#' @param score Vector series.
+#' @param k Maximum horizon.
+#' @return Autocovariance matrix
+#' @examples
+#' d <- 5
+#' n <- 1000
+#' vals <- matrix(norm(n * d), ncol = d)
+#' make_hac(vals, 5)
+#' @export
+#' @importFrom stats acf
 autocovariance_matrix <- function(score, k) {
   return(acf(score, lag.max = k, type = "covariance")$acf)
 }
 
+
+#' Autocorrelation-proof estimates using HAC estimator.
+#' @param acf_matrices Matrices to use in the estimators.
+#' @param near_pd Boolean, nearest positive definite matrix from HAC estimate.
+#' @return HAC-transformed matrix
+#' @examples
+#' d <- 5
+#' mats <- array(0, c(d, d, d))
+#' mats <- apply(mats, c(2, 3), function(x) {
+#'   diag(x) <- 1
+#'   return(x)
+#' })
+#' make_hac(mats)
 #' @importFrom Matrix nearPD
+#' @importFrom assertthat assert_that
+#' @export
 make_hac <- function(acf_matrices, near_pd = F) {
   # acf_matrices is k x n_params x n_params
   k <- dim(acf_matrices)[1]
