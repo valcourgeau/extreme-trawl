2c1ef904654a96d9b8687d97edc794de0e330f91
Valentin Courgeau <val.courgeau@gmail.com>
2021-08-29 14:12:12 +0100

    feat: tests for gamma and sup IG trawls

--- R/trawl_sup_ig.R
+++ R/trawl_sup_ig.R
@@ -1,29 +1,31 @@
 sup_ig_trawl <- new.env()
 
-# param is (gamma, delta)
-
 sup_ig_trawl$leb_a <- function(param) {
+  # param is (gamma, delta)
+  stopifnot(length(param) == 2)
+  stopifnot(all(param > 0))
   return(param[1] / param[2])
 }
 
 sup_ig_trawl$trawl_b_one <- function(param, h) {
-  return(
-    sup_ig_trawl$leb_a(param) *
-      (1 - exp(param[1] * param[2] *
-        (1 - sqrt(1 + 2 * h / param[1]^2)))
-      )
-  )
+  stopifnot(length(param) == 2)
+  stopifnot(all(param > 0))
+  a_total <- sup_ig_trawl$leb_a(param)
+  log_scaling_factor <- param[1] * param[2] * (1 - sqrt(1 + 2 * h / param[1]^2))
+  return(a_total * (1 - exp(log_scaling_factor)))
 }
 
 sup_ig_trawl$trawl_b_two <- function(param, h) {
-  return(
-    exp(param[1] * param[2] *
-      (1 - sqrt(1 + 2 * h / param[1]^2))) *
-      sup_ig_trawl$leb_a(param)
-  )
+  stopifnot(length(param) == 2)
+  stopifnot(all(param > 0))
+  a_total <- sup_ig_trawl$leb_a(param)
+  log_scaling_factor <- param[1] * param[2] * (1 - sqrt(1 + 2 * h / param[1]^2))
+  return(a_total * exp(log_scaling_factor))
 }
 
 sup_ig_trawl$trawl_b_three <- function(param, h) {
+  stopifnot(length(param) == 2)
+  stopifnot(all(param > 0))
   return(sup_ig_trawl$trawl_b_one(param, h))
 }
 
